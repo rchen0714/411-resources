c5220eef95b8bee7e3eb81fbcb95850f5eeb1d39
Ruby Chen <rc071404@bu.edu>
2025-02-11 11:13:00 -0500

    completely the last step 4 and used dockers healthcheck functionality. added screenshots of the container starting and when the container became unhealthy

--- HW/HW2 Docker/flask/app.py
+++ HW/HW2 Docker/flask/app.py
@@ -20,24 +20,4 @@ def repeat():
     user_input = request.args.get("input", "No input provided") 
     response = make_response(
         {
-            'body': user_input,
-            'status': 200
-        }
-    )
-    return response
-
-@app.route('/health')
-@app.route('/healthcheck')
-def health():
-    response = {
-        "body": "OK",
-        "status": 200,
-    }
-    return jsonify(response)
-
-
-if __name__ == '__main__':
-    # By default flask is only accessible from localhost.
-    # Set this to '0.0.0.0' to make it accessible from any IP address
-    # on your network (not recommended for production use)
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+            'body': user_inpu
